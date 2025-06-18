dfb9e5543d9e22af2b1a344669c2217d155f4155
Seungmin <rnsdls1997@gmail.com>
2025-06-18 14:14:48 +0900

    chore: 패키지 구조 변경

--- src/main/java/org/spring/example/home/HomeController.java
+++ src/main/java/org/spring/example/home/HomeController.java
@@ -1,4 +1,4 @@
-package org.spring.example.controller.home;
+package org.spring.example.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
